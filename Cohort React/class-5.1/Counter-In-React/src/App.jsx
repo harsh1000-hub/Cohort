8485aaaef923578d866ea5bc950bdf04909cd179
Harsh Vardhan <72147432+harsh1000-hub@users.noreply.github.com>
2024-01-13 13:50:04 +0530

    Tech 1

--- Cohort React/class-5.1/Counter-In-React/src/App.jsx
+++ Cohort React/class-5.1/Counter-In-React/src/App.jsx
@@ -1,20 +1,22 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  // local state 
-  const [count,setCount] = useState(0); // intial state is 0
+  // local state
+  const [count, setCount] = useState(0); // intial state is 0
   return (
     <div>
-    {/* Inside curly bracket we pass {count} this count is basically the count value that is in local state*/}
-     <Button count={count} setCount={setCount}></Button> {/* here passing setCount function as props in below button Component and same for count*/}
+      {/* Inside curly bracket we pass {count} this count is basically the count value that is in local state*/}
+      <Button count={count} setCount={setCount}></Button>{" "}
+      {/* here passing setCount function as props in below button Component and same for count*/}
     </div>
-  )
+  );
 }
-function Button(props){  // props is way to pass data from parent component to child component and vice-versa
-    function  onButtonClick(){
-      props.setCount(props.count+1); // here props.setCount => calling setCount method and here props.count+1 means we can access count value and update that value
-    }
-    return <button onClick={onButtonClick}>Counter {props.count}</button>
+function Button(props) {
+  // props is way to pass data from parent component to child component and vice-versa
+  function onButtonClick() {
+    props.setCount(props.count + 1); // here props.setCount => calling setCount method and here props.count+1 means we can access count value and update that value
+  }
+  return <button onClick={onButtonClick}>Counter {props.count}</button>;
 }
-export default App
+export default App;
